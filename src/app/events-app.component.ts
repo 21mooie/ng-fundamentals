ada9df1f97b53699078b585e3506c661cc7b34b7
Muata Nkosi <mnkosi@fandm.edu>
2018-11-06 19:51:45 -0500

    Getting Started with Angular

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app-root',
+  template: `<H2>HELLO WORLD</H2>
+            <img src="/assets/images/basic-shield.png"/>`
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
