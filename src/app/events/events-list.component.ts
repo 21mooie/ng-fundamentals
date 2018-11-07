50c20dc0037331e2b0a01397c2d65df2d7292008
Muata Nkosi <mnkosi@fandm.edu>
2018-11-06 22:45:52 -0500

    Communicate with child components using @Input

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,24 +1,16 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'events-list',
-    template: `
+    template:`
     <div>
         <h1>Upcoming Angular Events</h1>
-        <hr>
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
-            <span>{{event.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
-        </div>
+        <hr/>
+        <event-thumbnail [event]="event1"></event-thumbnail>
     </div>
     `
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
