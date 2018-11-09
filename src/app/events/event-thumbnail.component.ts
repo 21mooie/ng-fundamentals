28f860f93b9beb90ef17b63fef66bfe4b2fcabc5
Muata Nkosi <muata.nkosi@prudential.com>
2018-11-09 14:44:33 -0500

    Use template variables to interact with child components

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -19,8 +19,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 export class EventThumbnailComponent {
     @Input() event: any;
     @Output() eventClick = new EventEmitter();
+    someProperty: any = 'some value';
 
     handleClickMe(): void {
         this.eventClick.emit(this.event.name);
+    }
+
+    logFoo():void {
+        console.log('foo');
     }
 }
