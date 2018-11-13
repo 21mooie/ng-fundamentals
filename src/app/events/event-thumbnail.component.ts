c38f290e316dcde517941eaf21441650719d6f85
Muata Nkosi <muata.nkosi@prudential.com>
2018-11-13 14:26:18 -0500

    Style components with ngClass (ngClass receives object)

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
         <h2> {{event?.name}} </h2>
         <div>Date: {{event?.date}} </div>
-        <div [class.green]="event?.time === '8:00 am'"[ngSwitch]="event?.time">
+        <div [ngClass]="getStartTimeClass()"[ngSwitch]="event?.time">
             Time: {{event?.time}} (
             <span *ngSwitchCase="'8:00 am'">Early Start</span>
             <span *ngSwitchCase="'10:00 am'">Late Start</span>
@@ -27,6 +27,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     .green {
         color: #003300 !important;
     }
+    .bold {
+        font-weight: bold;
+    }
     .thumbnail{
         min-height:210px
     }
@@ -46,5 +49,11 @@ export class EventThumbnailComponent {
     handleClickMe(): void {
         this.eventClick.emit(this.event.name);
     }
+
+    getStartTimeClass(){
+       const isEarlyStart = this.event && this.event.time === '8:00 am';
+       return {green: isEarlyStart, bold: isEarlyStart}
+    }
+    
 
 }
