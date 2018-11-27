8736dda0bbb49e633a0cfc54d022ae5308d61bd4
Muata Nkosi <muata.nkosi@prudential.com>
2018-11-27 17:19:06 -0500

    Access route parameters

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from 'src/app/shared/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -8,10 +9,10 @@ import { EventService } from 'src/app/shared/event.service';
 export class EventDetailsComponent implements OnInit {
   event: any;
 
-  constructor(private eventService:EventService) { }
+  constructor(private eventService:EventService, private route:ActivatedRoute) { }
 
   ngOnInit() {
-   this.event = this.eventService.getEvent(1);
+   this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
   }
 
 }
